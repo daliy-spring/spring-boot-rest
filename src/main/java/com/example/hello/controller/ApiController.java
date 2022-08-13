144c1510c6e1e01b72028954ef98961e7f6ab8d0
Haneul Lee <hee1149kr@naver.com>
2022-08-13 16:57:24 +0900

    feat: response best practice (#5)

--- src/main/java/com/example/hello/controller/ApiController.java
+++ src/main/java/com/example/hello/controller/ApiController.java
@@ -1,8 +1,9 @@
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.hello.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // 해당 클래스는 Rest api를 처리하는 controller
 @RequestMapping("/api") // RequestMapping URI 를 지정해주는 어노테이션
@@ -11,4 +12,26 @@ public class ApiController {
     public String hello() {
         return "hello spring boot";
     }
+
+    //content-type: text/plain;charset=UTF-8
+    //Text를 리턴
+    @GetMapping("/text")
+    public String text(@RequestParam String account) {
+        return account;
+    }
+
+
+    //application/json
+    // req -> object mapper -> object -> method -> object -> object mapper -> json -> response
+    @PostMapping("/json")
+    public User json(@RequestBody User user){
+        return user;
+    }
+
+    // use ResponseEntity
+    @PutMapping("/put")
+    public ResponseEntity<User> put(@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
 }
---
+++ src/main/java/com/example/hello/dto/User.java
@@ -0,0 +1,62 @@
+package com.example.hello.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_NULL) // null 포함 x
+public class User {
+    private String name;
+    private Integer age;
+
+
+    private String phoneNumber;
+    private String address;
+
+
+    // object mapper는 get 메서드를 본다.
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
